0c6b53ece03d0a459534526e3f03b2973f948b85
BayuSwari01 <bayuswari.01@gmail.com>
2022-09-11 21:58:25 +0700

    latihan animated container

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -12,42 +13,31 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  List<Widget> widgets = [];
-  int counter = 1;
+  Random random = Random();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Latihan List View"),
-        ),
-        body: ListView(
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                RaisedButton(child: Text("Tambah Data"), onPressed: () {
-                  setState(() {
-                    widgets.add(Text("Data ke-" + counter.toString(), style: TextStyle(fontSize: 35),));
-                    counter++;
-                  });
-                }),
-                RaisedButton(child: Text("Hapus Data"), onPressed: () {
-                  setState(() {
-                    widgets.removeLast();
-                    counter--;
-                  });
-                }),
-              ],
+          appBar: AppBar(
+            title: Text("Latihan Animated Container"),
+          ),
+          body: Center(
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  
+                });
+              },
+              child: AnimatedContainer(
+                color: Color.fromARGB(255, random.nextInt(256),
+                    random.nextInt(256), random.nextInt(256)),
+                duration: Duration(seconds: 1),
+                width: 50.0 + random.nextInt(101),
+                height: 50.0 + random.nextInt(101),
+              ),
             ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: widgets,
-            )
-          ]
-        ),
-      ),
+          )),
     );
   }
 }
